44f9b0d4d18c670674275078c508d37a299f0cf3
mattsousatwo <mattsousa@mail.com>
2020-03-03 21:48:17 -0500

    added property to TaskData - cellPos
    - added a property to TaskData - cellPosition
    - recreated the subclass for TaskData

---
+++ FocusOn/TaskData+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  TaskData+CoreDataClass.swift
+//  FocusOn
+//
+//  Created by Matthew Sousa on 3/3/20.
+//  Copyright © 2020 Matthew Sousa. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TaskData)
+public class TaskData: NSManagedObject {
+
+}
